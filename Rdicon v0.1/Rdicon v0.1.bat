df99dc1ecfc00d33c579505ac6a6506f07a23111
AjaxBleach <31293181+AjaxBleach@users.noreply.github.com>
2017-11-01 23:03:50 -0400

    Rdicon v0.1
    Features
    -Add icon to removable media from a specific folder

---
+++ Rdicon v0.1/Rdicon v0.1.bat
@@ -0,0 +1,26 @@
+::Rdicon v0.1
+::Author: Mario Alanzo Junior Mills 1 Nov 2017
+setlocal EnableDelayedExpansion
+dir icon /b /a-d> listforicons.txt
+::
+set INPUT_FILE="listforicons.txt"
+
+:: # Count the number of lines in the text file and generate a random number
+for /f "usebackq" %%a in (`find /V /C "" ^< %INPUT_FILE%`) do set lines=%%a
+set /a randnum=%RANDOM% * lines / 32768 + 1, skiplines=randnum-1
+
+:: # Extract the line from the file
+set skip=
+if %skiplines% gtr 0 set skip=skip=%skiplines%
+for /f "usebackq %skip% delims=" %%a in (%INPUT_FILE%) do set "randline=%%a" & goto continue
+:continue
+
+set m1=[Autorun]
+set m2=icon^=icon\!randline!
+
+echo !m1!>Autorun.inf
+echo !m2!>>Autorun.inf
+del /Q /F listforicons.txt
+
+
+
---
+++ Rdicon v0.1/Readme.txt
@@ -0,0 +1,12 @@
+Rdicon v0.1 - 1 Nov 2017
+Author: Mario Alanzo Junior Mills
+
+Description:
+Gives removable storage (eg. USB) a random icon.
+
+Instructions:
+1.Bakup any existing Autorun.inf file.
+2.Place files in usb (not in a folder).
+3.Place your icons in the icon folder.
+4.Run RdiconV0.1.bat to generate Autorun.inf.
+5.Safely ejject and plug usb in and out of computer to see new icon.
